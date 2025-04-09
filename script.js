6e1e3a182213b7548a03f0c30c9aaadc11153ad8
Felix <Fatkidkiter@gmail.com>
2025-04-09 11:37:09 +0200

    zweiter commit, css angepasst etc

--- script.js
+++ script.js
@@ -1,20 +1,19 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Antwort anzeigen
-    document.querySelectorAll(".show-answer").forEach((button) => {
-      button.addEventListener("click", () => {
-        const answer = document.createElement("p");
-        answer.textContent = "Das ist die Antwort zur Frage.";
-        answer.classList.add("answer-text");
-        button.after(answer);
-        button.disabled = true;
-      });
-    });
-  
-    // Lesezeichen umschalten
-    document.querySelectorAll(".bookmark").forEach((btn) => {
-      btn.addEventListener("click", () => {
-        btn.classList.toggle("active");
-      });
+  // Antwort anzeigen
+  document.querySelectorAll(".show-answer").forEach((button) => {
+    button.addEventListener("click", () => {
+      const answer = document.createElement("p");
+      answer.textContent = "Antwort zur Frage.";
+      answer.classList.add("answer-text");
+      button.after(answer);
+      button.disabled = true;
     });
   });
-  
+
+  // Lesezeichen umschalten
+  document.querySelectorAll(".bookmark").forEach((btn) => {
+    btn.addEventListener("click", () => {
+      btn.classList.toggle("active");
+    });
+  });
+});
